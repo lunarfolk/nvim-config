80d5580cf1e07099e6bbc311624100643517fd1f
Cyril Berger <git@lunarfolk.fr>
2026-06-08 13:01:24 +0200

    chore: disable supermaven on .env files

--- lua/lunarfolk/plugins/supermaven.lua
+++ lua/lunarfolk/plugins/supermaven.lua
@@ -4,6 +4,10 @@ return {
         config = function()
             require("supermaven-nvim").setup({
                 ignore_filetypes = { markdown = true },
+                condition = function()
+                    local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t")
+                    return filename == ".env" or filename:match("^%.env%.") ~= nil
+                end,
             })
         end,
     },
